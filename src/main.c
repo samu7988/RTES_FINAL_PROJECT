151670468545c375fea12eb6e26045bc78ffedb5
samu7988 <samu7988@colorado.edu>
2022-04-22 20:55:31 -0600

    Get maximum and minimum priority of system

--- src/main.c
+++ src/main.c
@@ -60,6 +60,20 @@ cpu_set_t allcpuset;
  *********************************************************************************************/
 int main(int argc, char** argv)
 {
+
+    struct timeval current_time_val;
+    int i, rc, scope;
+    cpu_set_t threadcpu;
+    pthread_t threads[NUM_THREADS];
+    threadParams_t threadParams[NUM_THREADS];
+    pthread_attr_t rt_sched_attr[NUM_THREADS];
+    int rt_max_prio, rt_min_prio;
+    struct sched_param rt_param[NUM_THREADS];
+    struct sched_param main_param;
+    pthread_attr_t main_attr;
+    pid_t mainpid;
+    cpu_set_t allcpuset;
+
     if(argc < 2)
     {
         printf("\n\rInvalid Input: Usage is ./main <freq> <socket enable/disable>");
@@ -104,6 +118,29 @@ int main(int argc, char** argv)
     }
 
     printf("Using CPUS=%d from total available.\n", CPU_COUNT(&allcpuset));
-    
+
+    mainpid=getpid();
+
+    rt_max_prio = sched_get_priority_max(SCHED_FIFO);
+    rt_min_prio = sched_get_priority_min(SCHED_FIFO);
+
+    rc=sched_getparam(mainpid, &main_param);
+    main_param.sched_priority=rt_max_prio;
+    rc=sched_setscheduler(getpid(), SCHED_FIFO, &main_param);
+    if(rc < 0) perror("main_param");
+    print_scheduler();
+
+
+    pthread_attr_getscope(&main_attr, &scope);
+
+    if(scope == PTHREAD_SCOPE_SYSTEM)
+      printf("PTHREAD SCOPE SYSTEM\n");
+    else if (scope == PTHREAD_SCOPE_PROCESS)
+      printf("PTHREAD SCOPE PROCESS\n");
+    else
+      printf("PTHREAD SCOPE UNKNOWN\n");
+
+    printf("rt_max_prio=%d\n", rt_max_prio);
+    printf("rt_min_prio=%d\n", rt_min_prio);    
     return 0;
 }
